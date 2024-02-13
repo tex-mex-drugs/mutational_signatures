1e4de469ff137f91d3b3a619e5f9528144c8a9d0
AdrenalinJunkie <woollc007@gmail.com>
2024-02-13 10:53:21 +0000

    BI-2 more memory efficient concatenation

--- tools/process_raw_gsm.py
+++ tools/process_raw_gsm.py
@@ -18,25 +18,27 @@ def process_gsm_into_useful_dataframes(cosmic_gsm_address: str,
                                        sample_data_address,
                                        description="raw cosmic GSM file"):
     print("---Batch reading {desc} file from {address}---".format(desc=description, address=cosmic_gsm_address))
-    mutation_list = []
-    transcript_list = []
-    sample_list = []
     for chunk in pd.read_csv(cosmic_gsm_address, sep="\t", chunksize=100000):
         chunk = chunk.loc[chunk[GSM.HGVSG].str.strip().str[-2] == '>']
         mutation_data = chunk[[GSM.GENE_SYMBOL, GSM.COSMIC_GENE_ID, GSM.GENOMIC_MUTATION_ID, GSM.CHROMOSOME,
                                GSM.GENOME_START, GSM.GENOME_STOP, GSM.STRAND, GSM.HGVSG, GSM.GENOMIC_WT_ALLELE,
                                GSM.GENOMIC_MUT_ALLELE]].drop_duplicates()
+        mutation_data.to_csv(mutation_data_address, mode="a", sep="\t")
         transcript_data = chunk[[GSM.TRANSCRIPT_ACCESSION, GSM.GENOMIC_MUTATION_ID, GSM.MUTATION_CDS, GSM.MUTATION_AA,
                                  GSM.MUTATION_DESCRIPTION, GSM.HGVSP, GSM.HGVSC, GSM.MUTATION_ID]].drop_duplicates()
+        transcript_data.to_csv(transcript_data_address, mode="a", sep="\t")
         sample_data = chunk[[GSM.COSMIC_SAMPLE_ID, GSM.SAMPLE_NAME, GSM.COSMIC_PHENOTYPE_ID, GSM.GENOMIC_MUTATION_ID,
                              GSM.MUTATION_ZYGOSITY, GSM.LOH, GSM.MUTATION_SOMATIC_STATUS]].drop_duplicates()
+        sample_data.to_csv(sample_data_address, mode="a", sep="\t")
 
-        mutation_list.append(mutation_data)
-        transcript_list.append(transcript_data)
-        sample_list.append(sample_data)
-    mutation_df = concat_and_print(mutation_list, "mutations data")
-    transcript_df = concat_and_print(transcript_list, "transcript data")
-    sample_df = concat_and_print(sample_list, "sample data")
+    mutation_df = pd.read_csv(mutation_data_address, sep="\t").drop_duplicates()
+    transcript_df = pd.read_csv(transcript_data_address, sep="\t").drop_duplicates()
+    sample_df = pd.read_csv(sample_data_address, sep="\t").drop_duplicates()
+
+    print("---Removing duplicate rows---")
+    mutation_df.drop_duplicates(inplace=True)
+    transcript_df.drop_duplicates(inplace=True)
+    sample_df.drop_duplicates(inplace=True)
 
     print("---Successfully batch read {desc} file---".format(desc=description))
     deal_with_data(df=mutation_df, output_file=mutation_data_address, df_description="mutations dataframe")
