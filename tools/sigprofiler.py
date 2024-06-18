1d30fd98d0f48fe3115d55f6193a2079d5e73ee6
AdrenalinJunkie <woollc007@gmail.com>
2024-06-18 11:36:42 +0100

    BI-2 adding a test option that only does one phenotype

--- tools/sigprofiler.py
+++ tools/sigprofiler.py
@@ -8,7 +8,8 @@ genInstall.install('GRCh38')
 
 def find_mutational_signatures(filtered_gsm: pd.DataFrame,
                                output_dir: str,
-                               exome=False):
+                               exome=False,
+                               test=False):
     tag = "_genome" if not exome else "_exome"
     print("---Check if output directory exists or create output directory---")
     check_folder_exists_or_create(output_dir)
@@ -19,6 +20,20 @@ def find_mutational_signatures(filtered_gsm: pd.DataFrame,
     print("---Creating a vcf file for each sample---")
     create_vcfs_from_gsm(filtered_gsm, output_dir, phenotypes)
     print("---Running sigprofiler---")
+    if test:
+        phenotype = phenotypes[0]
+        print("---testing system with phenotype {phen}---".format(phen=phenotype))
+        Analyze.cosmic_fit(name_of_phenotype_directory(output_dir, phenotype),
+                           name_of_phenotype_directory(output_dir, phenotype) + "_results" + tag,
+                           input_type="vcf",
+                           context_type="96",
+                           collapse_to_SBS96=True,
+                           cosmic_version=3.4,
+                           exome=exome,
+                           genome_build="GRCh38")
+        print("---Successfully run sigprofiler for phenotype {phen}---".format(phen=phenotype))
+        return
+
     i = 0
     for phenotype in phenotypes:
         i += 1
@@ -38,7 +53,8 @@ def find_mutational_signatures(filtered_gsm: pd.DataFrame,
 def filter_and_run(cosmic_samples_address: str,
                    cosmic_gsm_address: str,
                    output_dir: str,
-                   filtered_gsm_output_address=""):
+                   filtered_gsm_output_address="",
+                   test=False):
     genome_samples = CosmicSamples(cosmic_samples_address, genome=True, exome=False)
     exome_samples = CosmicSamples(cosmic_samples_address, genome=False, exome=True)
     gsm_verify(cosmic_gsm_address)
@@ -46,6 +62,9 @@ def filter_and_run(cosmic_samples_address: str,
                                     genome_samples,
                                     lower_threshold=None,
                                     upper_threshold=None)
+    if test:
+        find_mutational_signatures(genome_gsm, output_dir, exome=False, test=True)
+        return
     exome_gsm = read_gsm_from_file(cosmic_gsm_address,
                                    exome_samples,
                                    lower_threshold=None,
