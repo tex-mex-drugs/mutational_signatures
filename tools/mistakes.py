a963420953d9d98f20688eb9b1ab00a0dd0a4ad9
AdrenalinJunkie <woollc007@gmail.com>
2024-06-27 16:36:32 +0100

    BI-2 bug

--- tools/mistakes.py
+++ tools/mistakes.py
@@ -9,7 +9,7 @@ def move_samples_to_one_folder(old_dir: str, new_dir):
     entries = os.listdir(old_dir)
     # Filter out and return only the directories
     folders = [entry for entry in entries if os.path.isdir(os.path.join(old_dir, entry))]
-    phenotype_folders = [entry for entry in folders if (entry.startswith("COSO" and not entry.endswith("results")))]
+    phenotype_folders = [entry for entry in folders if (entry.startswith("COSO") and not entry.endswith("results"))]
     samples = []
     for phenotype in phenotype_folders:
         phenotype_folder = os.path.join(old_dir, phenotype)
